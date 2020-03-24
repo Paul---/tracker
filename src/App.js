f2702c5fc8584717e09fe5b0b90212a68d2d26f5
MrDean <Paul---@users.noreply.github.com>
2020-03-24 14:22:41 -0600

    add basic components and pages with project organization in mind

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
+import Header from '../src/components/Header/Header';
+import HomePage from '../src/pages/HomePage/HomePage';
+import WelcomePage from '../src/pages/WelcomePage/WelcomePage';
+import SignupPage from '../src/pages/Signup/Signup';
+import LoginPage from '../src/pages/LoginPage/LoginPage';
 
 function App() {
   return (
-    <div>
+    <div className='container'>
+      <Header />
       <Switch>
-        <Route path="/about">
-          <About />
+        <Route path="/home">
+          <HomePage />
         </Route>
-        <Route path="/contact/:id">
-          <Contact />
+        <Route path="/signup">
+          <SignupPage />
         </Route>
-        <Route path="/contact">
-          <AllContacts />
+        <Route path="/login">
+          <LoginPage />
         </Route>
-        <Route path="/">
-          <Home />
+        <Route exact path="/">
+          <WelcomePage />
         </Route>
       </Switch>
     </div>
